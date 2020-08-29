652dd9913f8e4fab2a5fada868ae6c712e07ab22
basyoukajiki <okaki.cs@gmail.com>
2020-08-29 15:57:37 +0900

    create FizzBuzz

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # codeal-tryout
 The tryout project
+
+ターミナル上で
+ruby main.rb
+と実行していただければ幸いです。
---
+++ main.rb
@@ -0,0 +1,15 @@
+def fizz_buzz
+  for i in 1..100 do
+    if i % 5 == 0 &&  i % 3 == 0
+      puts "FizzBuzz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    elsif i % 3 ==0
+      puts "Fizz"
+    else
+      puts i
+    end
+  end
+end
+
+fizz_buzz
