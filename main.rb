f0581748a8fbdd16d624b899669aae93b072c920
MinoruKotegawa <kotegawa.1029@gmail.com>
2020-09-08 14:29:06 +0900

    codeal test

---
+++ main.rb
@@ -0,0 +1,11 @@
+for num in 1..100 do
+  if num % 15 == 0 then
+    puts "FizzBuzz"
+  elsif num % 3 == 0 then
+    puts "Fizz"
+  elsif num % 5 == 0 then
+    puts "Buzz"
+  else
+    puts num
+  end
+end
